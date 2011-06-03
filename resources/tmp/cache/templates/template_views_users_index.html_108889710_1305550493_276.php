0a4dd00dc0fa13cb9fd565b3ddff7e6a810b1616
Johan Sonesson <johan@tteknik.se>
2011-06-03 22:13:14 +0200

    removing cache files

--- resources/tmp/cache/templates/template_views_users_index.html_108889710_1305550493_276.php
+++
@@ -1,8 +0,0 @@
-<table>
-<?php foreach ($users as $user) 
-{ ?>
-	<tr>
-    <td><?php echo $h($user->username); ?></td><td><?php echo $this->html->link('Ändra', "users/edit/{$user->id}"); ?></td><td><?php echo $this->html->link('Ändra Lösenord', "users/password/{$user->id}"); ?></td>
-    </tr>
-<?php } ?>
-</table>
