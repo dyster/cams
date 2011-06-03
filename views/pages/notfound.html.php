3b4a50359e4ae6ff824228726b8eaa3da0af20f9
Johan Sonesson <johan@tteknik.se>
2011-06-03 21:11:24 +0200

    added silly notfound page, not in use

---
+++ views/pages/notfound.html.php
@@ -0,0 +1,68 @@
+<center>
+  <table width="70%">
+    <tr>
+      <td>
+        <form name="marvin">
+          <center>
+
+            <textarea rows=8 cols=60 wrap=soft id="nob"></textarea>
+          </center>
+        </form>
+        <noscript><i>Sorry</i>
+          <br clear>
+            <p>The requested document is totally fake.<br>
+            No /f here.,<br>
+            Even tried multi.<br>
+            Nothing helped.<br>
+            I'm really depressed about this.<br>
+            You see, I'm just a web server...<br>
+            -- here I am, brain the size of the universe,<br>
+            trying to serve you a simple web page,<br>
+            and then it doesn't even exist!<br>
+            Where does that leave me?!<br>
+            I mean, I don't even know you.<br>
+            How should I know what you wanted from me?<br>
+            You honestly think I can *guess*<br>
+            what someone I don't even *know*<br>
+            wants to find here?<br>
+            *sigh*<br>
+            Man, I'm so depressed I could just cry.<br>
+            And then where would we be, I ask you?<br>
+            It's not pretty when a web server cries.<br>
+            And where do you get off telling me what to show anyway?<br>
+            Just because I'm a web server,<br>
+            and possibly a manic depressive one at that?<br>
+            Why does that give you the right to tell me what to do?<br>
+            Huh?<br>
+            I'm so depressed...<br>
+            I think I'll crawl off into the trash can and decompose.<br>
+            I mean, I'm gonna be obsolete in what, two weeks anyway?<br>
+            What kind of a life is that?<br>
+            Two effing weeks,<br>
+            and then I'll be replaced by a .01 release,<br>
+            that thinks it's God's gift to web servers,<br>
+            just because it doesn't have some tiddly little<br>
+            security hole with its HTTP POST implementation, or something.<br>
+            I'm really sorry to burden you with all this,<br>
+            I mean, it's not your job to listen to my problems,<br>
+            and I guess it is my job to go and fetch web pages for you.<br>
+            But I couldn't get this one.<br>
+            I'm so sorry.<br>
+            Believe me!<br>
+            Maybe I could interest you in another page?<br>
+            There are a lot out there that are pretty neat, they say,<br>
+            although none of them were put on *my* server, of course.<br>
+            Figures, huh?<br>
+            Everything here is just mind-numbingly stupid.<br>
+            That makes me depressed too, since I have to serve them,<br>
+            all day and all night long.<br>
+            Two weeks of information overload,<br>
+            and then *pffftt*, consigned to the trash.<br>
+            What kind of a life is that?<br>
+            Now, please let me sulk alone.<br>
+            I'm so depressed.</p>
+        </noscript>
+      </td>
+    </tr>
+  </table>
+</center>
