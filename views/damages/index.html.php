3dbbbc8b532abda95d2855db1b73eb1780415fea
Johan Sonesson <johan@tteknik.se>
2012-01-28 23:25:58 +0100

    layout adjustments

--- views/damages/index.html.php
+++ views/damages/index.html.php
@@ -4,11 +4,12 @@
 <table>
 	<thead>
 		<tr>
-			<th style="width: 100px;">Inlagd</th><th style="width: 20px;">Prio</th><th>Fordon</th><th>Grupp</th><th>Skada</th><th>Inlagd av</th>
+			<th style="width: 20px;">ID</th><th style="width: 110px;">Inlagd</th><th style="width: 20px;">Prio</th><th style="width: 150px;">Fordon</th><th style="width: 20px;">Grupp</th><th>Skada</th><th style="width: 120px;">Inlagd av</th>
 		</tr>
 	</thead>
 	<?php foreach($damages as $damage) { $object = $damage->getObject(); ?>
 	<tr>
+		<td><?=$damage->id;?></td>
 		<td><?=substr($damage->created, 0, 16);?></td>
 		<td style="<?php switch($damage->prio)
 	{
@@ -23,20 +24,57 @@
 	</tr>	
 	<?php } ?>
 	<tfoot>
-		<tr><td colspan="6">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/$a/20/20").' '; ?></td></tr>
+		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/$a/10/10").' '; ?></td></tr>
 	</tfoot>	
 </table>
+
+
+
+<h2>Senast kvitterade skador</h2>
+
+<table>
+	<thead>
+		<tr>
+			<th style="width: 20px;">ID</th><th style="width: 110px;">Kvitterad</th><th style="width: 20px;">Prio</th><th style="width: 150px;">Fordon</th><<th style="width: 20px;">Grupp</th><th>Skada</th><th style="width: 120px;">Av</th>
+		</tr>
+	</thead>
+	<?php foreach($nulleddamages as $damage) { $object = $damage->getObject(); ?>
+	<tr>
+		<td><?=$damage->id;?></td>
+		<td><?=substr($damage->nulled, 0, 16);?></td>
+		<td style="<?php switch($damage->prio)
+	{
+		case (1): echo 'background: #fd8888;'; break;
+		case (2): echo 'background: #3bb9ff;'; break;
+		default: echo ''; break;
+	} ?>"><?=$damage->prio;?></td>
+		<!--<td><?=(strtotime($damage->modified) > 0)?$damage->modified:"N/A";?></td>-->
+		<td> <?=$this->html->link($object->toString(), 'objects/view/'.$object->id);?></td>
+		<td><?=$object->group;?></td>
+		<td><?=$this->html->link($damage->short, 'damages/view/'.$damage->id);?></td>
+		
+		
+		<td><?=$damage->getNulledBy()->fullname;?></td>
+	</tr>	
+	<?php } ?>	
+	<tfoot>
+		<tr><td colspan="8">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/10/$a").' '; ?></td></tr>
+	</tfoot>
+</table>
+
 
 <h2>Senast ändrade skador</h2>
 
 <table>
 	<thead>
 		<tr>
-			<th style="width: 100px;">Inlagd</th><th style="width: 20px;">Prio</th><th>Fordon</th><th>Grupp</th><th>Skada</th><th>Ändrad av</th>
+			<th style="width: 20px;">ID</th><th style="width: 110px;">Inlagd</th><th style="width: 20px;">Prio</th><th style="width: 150px;">Fordon</th><th style="width: 20px;">Grupp</th><th>Skada</th><th style="width: 120px;">Ändrad av</th>
 		</tr>
 	</thead>
+	<tbody>
 	<?php foreach($modifieddamages as $damage) { $object = $damage->getObject(); ?>
 	<tr>
+		<td><?=$damage->id;?></td>
 		<td><?=substr($damage->created, 0, 16);?></td>
 		<td style="<?php switch($damage->prio)
 	{
@@ -50,40 +88,10 @@
 		
 		<td><?=$damage->getModifiedBy()->fullname;?></td>
 	</tr>	
-	<?php } ?>	
+	<?php } ?>
+	</tbody>	
 	<tfoot>
-		<tr><td colspan="6">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/20/$a/20").' '; ?></td></tr>
-	</tfoot>
-</table>
-
-<h2>Senast kvitterade skador</h2>
-
-<table>
-	<thead>
-		<tr>
-			<th style="width: 100px;">Inlagd</th><th style="width: 20px;">Prio</th><th>Fordon</th><th>Grupp</th><th>Skada</th><th style="width: 100px;">Kvitterad</th><th>Av</th>
-		</tr>
-	</thead>
-	<?php foreach($nulleddamages as $damage) { $object = $damage->getObject(); ?>
-	<tr>
-		<td><?=substr($damage->created, 0, 16);?></td>
-		<td style="<?php switch($damage->prio)
-	{
-		case (1): echo 'background: #fd8888;'; break;
-		case (2): echo 'background: #3bb9ff;'; break;
-		default: echo ''; break;
-	} ?>"><?=$damage->prio;?></td>
-		<!--<td><?=(strtotime($damage->modified) > 0)?$damage->modified:"N/A";?></td>-->
-		<td> <?=$this->html->link($object->toString(), 'objects/view/'.$object->id);?></td>
-		<td><?=$object->group;?></td>
-		<td><?=$this->html->link($damage->short, 'damages/view/'.$damage->id);?></td>
-		
-		<td><?=substr($damage->nulled, 0, 16);?></td>
-		<td><?=$damage->getNulledBy()->fullname;?></td>
-	</tr>	
-	<?php } ?>	
-	<tfoot>
-		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/20/20/$a").' '; ?></td></tr>
+		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/$a/10").' '; ?></td></tr>
 	</tfoot>
 </table>
 
