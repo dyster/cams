e199a30739b2107d4566d3482ed285ecc295f840
Johan Sonesson <johan@tteknik.se>
2012-02-20 14:46:44 +0100

    Touch up

--- views/damages/index.html.php
+++ views/damages/index.html.php
@@ -28,14 +28,12 @@
 	</tfoot>	
 </table>
 
-
-
 <h2>Senast kvitterade skador</h2>
 
 <table>
 	<thead>
 		<tr>
-			<th style="width: 20px;">ID</th><th style="width: 110px;">Kvitterad</th><th style="width: 20px;">Prio</th><th style="width: 150px;">Fordon</th><<th style="width: 20px;">Grupp</th><th>Skada</th><th style="width: 120px;">Av</th>
+			<th style="width: 20px;">ID</th><th style="width: 110px;">Kvitterad</th><th style="width: 20px;">Prio</th><th style="width: 150px;">Fordon</th><th style="width: 20px;">Grupp</th><th>Skada</th><th style="width: 120px;">Av</th>
 		</tr>
 	</thead>
 	<?php foreach($nulleddamages as $damage) { $object = $damage->getObject(); ?>
@@ -58,7 +56,7 @@
 	</tr>	
 	<?php } ?>	
 	<tfoot>
-		<tr><td colspan="8">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/10/$a").' '; ?></td></tr>
+		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/10/$a").' '; ?></td></tr>
 	</tfoot>
 </table>
 
