2b734d9c5c30768f47964665b081c3b43716ebb2
Johan Sonesson <johan@tteknik.se>
2012-03-02 22:11:13 +0100

    style changes

--- views/damages/index.html.php
+++ views/damages/index.html.php
@@ -11,7 +11,7 @@
 	<tr>
 		<td><?=$damage->id;?></td>
 		<td><?=substr($damage->created, 0, 16);?></td>
-		<td style="<?php switch($damage->prio)
+		<td style="text-align: center; <?php switch($damage->prio)
 	{
 		case (1): echo 'background: #fd8888;'; break;
 		case (2): echo 'background: #3bb9ff;'; break; // gul = #fbf678
@@ -21,11 +21,11 @@
 		<td><?=$object->group;?></td>
 		<td><?=$this->html->link($damage->short, 'damages/view/'.$damage->id);?></td>
 		<td><?=$damage->getCreatedBy()->fullname;?></td>
-	</tr>	
+	</tr>
 	<?php } ?>
 	<tfoot>
 		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/$a/10/10").' '; ?></td></tr>
-	</tfoot>	
+	</tfoot>
 </table>
 
 <h2>Senast kvitterade skador</h2>
@@ -40,7 +40,7 @@
 	<tr>
 		<td><?=$damage->id;?></td>
 		<td><?=substr($damage->nulled, 0, 16);?></td>
-		<td style="<?php switch($damage->prio)
+		<td style="text-align: center; <?php switch($damage->prio)
 	{
 		case (1): echo 'background: #fd8888;'; break;
 		case (2): echo 'background: #3bb9ff;'; break;
@@ -50,11 +50,11 @@
 		<td> <?=$this->html->link($object->toString(), 'objects/view/'.$object->id);?></td>
 		<td><?=$object->group;?></td>
 		<td><?=$this->html->link($damage->short, 'damages/view/'.$damage->id);?></td>
-		
-		
+
+
 		<td><?=$damage->getNulledBy()->fullname;?></td>
-	</tr>	
-	<?php } ?>	
+	</tr>
+	<?php } ?>
 	<tfoot>
 		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/10/$a").' '; ?></td></tr>
 	</tfoot>
@@ -74,7 +74,7 @@
 	<tr>
 		<td><?=$damage->id;?></td>
 		<td><?=substr($damage->created, 0, 16);?></td>
-		<td style="<?php switch($damage->prio)
+		<td style="text-align: center; <?php switch($damage->prio)
 	{
 		case (1): echo 'background: #fd8888;'; break;
 		case (2): echo 'background: #3bb9ff;'; break;
@@ -83,11 +83,11 @@
 		<td> <?=$this->html->link($object->toString(), 'objects/view/'.$object->id);?></td>
 		<td><?=$object->group;?></td>
 		<td><?=$this->html->link($damage->short, 'damages/view/'.$damage->id);?></td>
-		
+
 		<td><?=$damage->getModifiedBy()->fullname;?></td>
-	</tr>	
+	</tr>
 	<?php } ?>
-	</tbody>	
+	</tbody>
 	<tfoot>
 		<tr><td colspan="7">Visa <?php foreach($antal as $a) echo $this->html->link($a, "/damages/index/10/$a/10").' '; ?></td></tr>
 	</tfoot>
